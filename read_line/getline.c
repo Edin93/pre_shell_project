f911611b82d70fb5ac1545bf3eb37ba70c1a770f
edin93 <il_matador@hotmail.fr>
2019-11-20 15:02:21 +0000

    changed getline for tests

--- read_line/getline.c
+++ read_line/getline.c
@@ -4,12 +4,15 @@ int main(void)
 {
 	char *lineptr = NULL;
 	size_t s = 0;
-	ssize_t r;
+	ssize_t chars_num; /* number of characters, to store getline result */
 
 	printf("$ ");
-	r = getline(&lineptr, &s, stdin);
-	printf("%s", lineptr);
-	printf("getline result is: %zu\n", r);
+	chars_num = getline(&lineptr, &s, stdin);
+	while (chars_num >= 0)
+	{
+		printf("%s", lineptr);
+		chars_num = getline(&lineptr, &s, stdin);
+	}
 	free(lineptr);
 	return (0);
 }
