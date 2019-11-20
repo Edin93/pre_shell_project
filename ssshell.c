0aed76a6cf5e8dad9937a57b048f46ffc5dacc54
edin93 <il_matador@hotmail.fr>
2019-11-20 19:17:37 +0000

    super simple shell done

--- ssshell.c
+++ ssshell.c
@@ -1,20 +1,45 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-/**
- * main - runs commands with their full path, without any argument.
- * @ac: arguments count.
- * @av: arguments vector.
- * @envp: environment.
- * Return: int.
- */
-int main (int ac, char *av[], char *envp[])
-{
-	ssize_t size;
-	size_t n = ;
+#include <string.h>
 
-	size = getline(lineptr, &n, NULL);
+int main(void)
+{
+	/* getline vars */
+	char *lineptr = NULL;
+	size_t s = 0;
+	ssize_t chars_num; /* number of characters, to store getline result */
+	/* strtok vars */
+	char *token;
+	int i = 0;
+	char **words;
+	/* execve vars */
+	pid_t c;
+	int status;
+
 	printf("$ ");
+	getline(&lineptr, &s, stdin);
+	token = strtok(lineptr, " \n");
+	words = malloc(sizeof(char *));
+	while (token != NULL)
+	{
+		words = realloc(words, sizeof(char *) * (i + 1));
+		*(words + i) = strdup(token);
+		i++;
+		token = strtok(NULL, " \n");
+	}
+
+	c = fork();
+	if (c == 0)
+	{
+		if (execve(*words, words, NULL) == -1)
+			perror("Error executing the command\n");
+	}
+	if (wait(&status) == -1)
+	{
+		printf("Error at status: %d\n", status);
+		exit (EXIT_FAILURE);
+	}
+
+	free(lineptr);
+	return (0);
 }
