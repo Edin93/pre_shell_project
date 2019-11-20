f88e5af0b569385e9031463833d7297e6e369d19
edin93 <il_matador@hotmail.fr>
2019-11-20 15:05:22 +0000

    add getline example of reading from a file

--- read_line/getline.c
+++ read_line/getline.c
@@ -7,12 +7,18 @@ int main(void)
 	ssize_t chars_num; /* number of characters, to store getline result */
 
 	printf("$ ");
+
+	getline(&lineptr, &s, stdin);
+	printf("%s", lineptr);
+/*
+	==> this is example to read from file. (change stdin with file path)
 	chars_num = getline(&lineptr, &s, stdin);
 	while (chars_num >= 0)
 	{
 		printf("%s", lineptr);
 		chars_num = getline(&lineptr, &s, stdin);
 	}
+*/
 	free(lineptr);
 	return (0);
 }
